4d635f6b9dc257c8cda06d31e480ed2cf6b55f7f
taewoo-dev <twin9774@gmail.com>
2024-07-30 15:04:18 +0900

    feat: added app/index view

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    # 길이 제한 있음
+    title = models.CharField(max_length=100)
+    # 길이제한 없음
+    content = models.TextField()
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from app.models import Post
+
+
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    # qs = [
+    #     {"id": 1, "title": "post #1"},
+    #     {"id": 2, "title": "post #2"},
+    # ]
+    # render는 세번째 인자로 Queryset을 전달
+    return render(
+        request,
+        "app/index.html",
+        {
+            "post_list": qs,
+        },
+    )

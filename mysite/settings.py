5c7ba8a3cf95b4f19c99020117b073f6e6b95d41
taewoo-dev <twin9774@gmail.com>
2024-08-04 15:49:27 +0900

    feat: accounts app 생성 및 등록

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # loacl apps
     "app",
     "catube",
+    "accounts",
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views as app_views
 
 # 뷰 함수 등록
@@ -24,4 +24,5 @@ urlpatterns = [
     path("app/", app_views.index),
     path("app/<int:pk>", app_views.post_detail),
     path("app/new/", app_views.post_new),
+    path("accounts/", include("accounts.urls")),
 ]
